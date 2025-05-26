55ca7295d2df6b0500d0c0c7ce4fc0d0f7817f65
padamsandhu <sandhp7@gmail.com>
2025-05-26 16:38:06 +0000

    Updated footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
         <TempoInit />
       </body>
     </html>
